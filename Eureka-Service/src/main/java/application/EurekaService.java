163d1fc46d432c257c5698bb86143f203c23fdab
Felix Fischer <felix.ma.fischer@gmail.com>
2019-01-07 19:17:36 +0000

    added documentation to module "Eureka-Service"

--- Eureka-Service/src/main/java/application/EurekaService.java
+++ Eureka-Service/src/main/java/application/EurekaService.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * {@link SpringBootApplication} that is the EUREKA Server.
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaService {
-
     public static void main(String[] args) {
         // start spring application
         SpringApplication.run(EurekaService.class, args);
